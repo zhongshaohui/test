e502da03a583e102a91e78fccb50bed7772a2fce
zhongshaohui <401447189@qq.com>
2020-06-02 22:29:16 +0800

    本地工程添加到仓库

---
+++ empsystem/src/main/webapp/myVue.vue
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Vue 测试实例 - 菜鸟教程(runoob.com)</title>
+    <script src="https://cdn.staticfile.org/vue/2.2.2/vue.min.js"></script>
+</head>
+<body>
+<div id="my">
+    <text {{message}}></text>
+    </div>
+<script>
+   new Vue(
+       {
+           el:"my",
+           data:{
+               message:"zhongshaohui"
+           }
+       }
+   )
+
+</script>
+</body>
+</html>
+
